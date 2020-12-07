38d450a659dd3becced7e16ef9b4fd8f9521aa0c
bacxhus <enricxpieri@gmail.com>
2020-12-07 17:17:51 +0000

    the trials

--- frontend/App.js
+++ frontend/App.js
@@ -1,29 +1,18 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './styles/style.scss'
-import axios from 'axios'
+
 
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
 import Settings from './components/Settings'
 import ProfileMatch from './components/ProfileMatch'
+import Swipe from './components/Swipe'
 
 // ! Some starter code for your frontends, change this
 // ! however you like.
 const App = () => {
-  // Getting list of all users to be used throughout the app. 
-  const token = localStorage.getItem('token')
-
-  useEffect(() => {
-    axios.get('/api/users', {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(resp => {
-        localStorage.setItem('UsersLength', resp.data.length)
-      })
-      .catch(err => console.log(err))
-  }, [])
 
   return <BrowserRouter>
     <Switch>
@@ -32,6 +21,7 @@ const App = () => {
       <Route exact path="/login" component={Login} />
       <Route exact path="/settings" component={Settings} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/swipe" component={Swipe} />
     </Switch>
   </BrowserRouter>
 }
