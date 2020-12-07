7612cd9bdb327e5b33717cef17830d5849f90a89
Rachel <rachellsmith.107@gmail.com>
2020-12-07 09:45:07 +0000

    Monday morning

--- frontend/components/ProfileMatch.js
+++ frontend/components/ProfileMatch.js
@@ -4,6 +4,7 @@ import { getUserId } from '../lib/auth'
 
 const ProfileMatch = (props) => {
   const [user, updateUser] = useState({})
+  const [userId, updateUserId] = useState({})
   const token = localStorage.getItem('token')
   const userLength = Number(localStorage.getItem('UsersLength'))
   const currentProfile = props.match.params.profileId
@@ -13,18 +14,46 @@ const ProfileMatch = (props) => {
   const [matchesInfo, updateMatchesInfo] = useState([])
 
   // * Const to generate a random user for the next match. 
-  const randomUser = Math.floor(Math.random() * userLength + 1)
-
-  console.log(randomUser)
-  // const [randomUser, updateRandomUser] = useState(0)
-  console.log(userLength)
 
 
-  // * Function that handles the information needed for POSTing to our likes table. 
+  const [randomUser, updateRandomUser] = useState(Number)
+
+  // * Function to generate a random user for the next match. 
+  const createRandom = () => {
+    updateRandomUser(Math.floor(Math.random() * userLength + 1))
+  }
+
+  // * Checks that the random user is not the current user. If it is, it will generate a new 
+  // * random user until it is not a match. 
+  {
+    getUserId() === randomUser ?
+      createRandom
+      :
+      console.log('not matching ID - continue')
+  }
+
+  useEffect(() => {s
+    getProfileInfo()
+    getMatchInfo()
+    createRandom()
+  }, [])
+
+  // * GETting the information on the current random user. 
+  function getProfileInfo() {
+    axios.get(`/api/users/${currentProfile}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(resp => {
+        updateUser(resp.data)
+        updateUserId(resp.data.id)
+      })
+      .catch(err => console.log(err))
+  }
+
+  // * Function that handles the information needed to POST to our likes table. 
   function handleChange(event) {
     const name = event.target.name
     const value = event.target.value
-
     const data = {
       ...likeData,
       [name]: value
@@ -33,26 +62,8 @@ const ProfileMatch = (props) => {
     handleLike()
   }
 
-  // * Pulling the information on the current random user. 
-  useEffect(() => {
-    axios.get(`/api/users/${currentProfile}`, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(resp => {
-        updateUser(resp.data)
-      })
-      .catch(err => console.log(err))
-  }, [])
-
-  // {
-  //   getUserId === randomUser ?
-  //     console.log('UserId and dating profile ID matched - to generate another random number')
-  //     :
-  //     console.log('not matching ID - continue')
-  // }
-
   // * Function that handles when a user 'likes' the profile. 
-  // * This pushs a POST request to our likes Table. 
+  // * This pushs a POST request to the likes Table. 
   function handleLike() {
     axios.post('/api/likes', likeData, {
       headers: { Authorization: `Bearer ${token}` }
@@ -66,13 +77,13 @@ const ProfileMatch = (props) => {
       })
   }
 
-  // * Function to reload the matches page with the next random user. 
+  // * Function that loads the next 'match' page, after the YES or NO button haa
   function nextProfile() {
     props.history.push(`/match/${randomUser}`)
     location.reload()
   }
 
-  useEffect(() => {
+  function getMatchInfo() {
     axios.get('/api/matches', {
       headers: { Authorization: `Bearer ${token}` }
     })
@@ -80,12 +91,36 @@ const ProfileMatch = (props) => {
         updateMatchesInfo(resp.data)
       })
       .catch(err => console.log(err))
-  }, [])
+    console.log(matchesInfo)
+  }
 
-  console.log(matchesInfo)
+  const matchOne = matchesInfo.map(match => {
+    return match.match_one_id
+  })
 
+  const matchTwo = matchesInfo.map(match => {
+    return match.match_two_id
+  })
 
+  const stringedOne = matchOne.toString()
 
+  console.log(stringedOne)
+  // function findingMatch() {
+  //   if (matchTwo === userId) {
+  //     return console.log('its a match')
+  //   } else {
+  //     return console.log('no match yet!')
+  //   }
+  // }
+
+  // findingMatch()
+  // console.log(matchTwo)
+  // console.log(userId)
+
+  // Expected to pick only firstname and age keys
+  // console.log(
+  //   filter(matchesInfo, 'match_one_id', 'match_two_id')
+  // )
 
   return <main>
     <h1>{user.username}</h1>
@@ -97,6 +132,8 @@ const ProfileMatch = (props) => {
       value={likeData.liked_id = user.id}
       name="liked_id"
     >YES</button>
+
+
   </main>
 }
 
